ea96ba27680b95718f45e4669140056bb837cb91
QuanPT <quanpt@gmail.com>
2021-02-25 01:25:16 +1100

    added quiz and questions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Question(props) {
+  return (
+    <div>
+      Question: {props.title}
+      Answer: xxx
+    </div>
+  );
+}
+
+class Quiz extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      title: 'Quiz title',
+      questions: Array(5).fill({
+        title: 'question text',
+        answers: [
+          {
+            title: 'a1', 
+            isCorrect: false,
+          },
+          {
+            title: 'a2', 
+            isCorrect: true,
+          },
+          {
+            title: 'a3', 
+            isCorrect: false,
+          },
+        ],
+      }),
+    };
+  }
+  renderQuestions() {
+
+    return (
+      <ul>
+        {this.state.questions.map((question, index) => {
+          return <li key={index}><Question question={question}/></li>
+        })}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Quiz</h1>
+        <div>
+          {this.renderQuestions()}
+        </div>
+      </div>
+    );
+  }
+}
+
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Quiz />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
