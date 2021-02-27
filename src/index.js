de4231196ecdfdeec1c1be6c42e47c5d2aa364ea
QuanPT <quanpt@gmail.com>
2021-02-28 01:18:22 +1100

    Added post result

--- src/index.js
+++ src/index.js
@@ -101,8 +101,7 @@ class Quiz extends React.Component {
               
               if (this.state.isSubmitted)
                 return;
-              
-              await new Promise((r) => setTimeout(r, 0));
+
               console.log(JSON.stringify(answers, null, 2));
               let newQuestions = this.state.questions.slice();
               let countCorrect = 0;
@@ -118,7 +117,25 @@ class Quiz extends React.Component {
               this.setState({
                 isSubmitted: true,
                 questions: newQuestions,
+                datetime: new Date(),
               });
+
+              const response = await fetch(process.env.PUBLIC_URL + "/mock_response.json", {
+                method: 'POST',
+                body: JSON.stringify(this.state, null, 2),
+                headers: {'Content-Type': 'application/json; charset=UTF-8'} });
+
+              if (!response.ok) { /* Handle */ }
+
+              // If you care about a response:
+              if (response.body !== null) {
+                // body is ReadableStream<Uint8Array>
+                // parse as needed, e.g. reading directly, or
+                const asString = new TextDecoder("utf-8").decode(response.body);
+                // and further:
+                const asJSON = JSON.parse(asString);  // implicitly 'any', make sure to verify type on runtime.
+                console.log(asJSON);
+              }
 
               alert("Total: " + this.state.questions.length + "\nAttempt: " + Object.keys(answers).length + "\nCorrect: " + countCorrect);
             }}
