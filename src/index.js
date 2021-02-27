0999afc72ed1106d4075fee4c6ad472eaab8d2e6
QuanPT <quanpt@gmail.com>
2021-02-28 00:45:50 +1100

    Highlighted correct and incorrect answers

--- src/index.js
+++ src/index.js
@@ -7,10 +7,20 @@ const CORRECT_LABEL = "@@@";
 
 
 function Answer(props) {
+  var answer = props.answer;
+  var isSubmitted = props.isSubmitted;
   return (
       <label>
-        <Field type="Radio" name={props.questionKey} value={props.answer.isCorrect ? CORRECT_LABEL : ("" + props.index)} disabled={props.isSubmitted}/>
-        <span className={"answer " + (props.isSubmitted ? (props.answer.isCorrect ? "correctAnswer" : "incorrectAnswer") : "")}>
+        <Field type="Radio" name={props.questionKey} value={("" + props.index)} disabled={isSubmitted}/>
+        <span className={
+          "answer " + (
+            isSubmitted ? (
+              answer.isSelected ? (
+                answer.isCorrect ? "correctAnswer" : "incorrectAnswer"
+              ) : ""
+            ) : ""
+          )
+        }>
           {String.fromCharCode(65 + props.index)}. {props.answer.title}
         </span>
       </label>
@@ -43,7 +53,7 @@ class Quiz extends React.Component {
     for (let i = 0; i < state.questions.length; i ++) {
       state.questions[i] = {
         title: 'question text ' + (i+1),
-        id: i+1,
+        id: "q" + (i+1),
         answers: [
           {
             title: 'a1' + (i+1), 
@@ -68,7 +78,7 @@ class Quiz extends React.Component {
     return (
       <div>
         {this.state.questions.map((question, index) => {
-          return <div key={"q_"+index}><Question question={question} isSubmitted={this.state.isSubmitted}/></div>
+          return <Question key={index} question={question} isSubmitted={this.state.isSubmitted}/>
         })}
       </div>
     );
@@ -86,21 +96,24 @@ class Quiz extends React.Component {
               return;
             
             await new Promise((r) => setTimeout(r, 0));
-            // alert(JSON.stringify(answers, null, 2));
+            console.log(JSON.stringify(answers, null, 2));
             let newQuestions = this.state.questions.slice();
-            for (let question in newQuestions) {
-              // question.
+            let countCorrect = 0;
+
+            for (let key in newQuestions) {
+              let question = newQuestions[key];
+              console.log(question.id + " " + answers[question.id]);
+              if (answers[question.id]) {
+                question.answers[answers[question.id]].isSelected = true;
+                countCorrect += question.answers[answers[question.id]].isCorrect ? 1 : 0;
+              }
             }
             this.setState({
               isSubmitted: true,
               questions: newQuestions,
             });
 
-            let count = 0;
-            for (let answer in answers) {
-              count += answers[answer] === CORRECT_LABEL ? 1 : 0;
-            }
-            alert("Total: " + this.state.questions.length + "\nAttempt: " + Object.keys(answers).length + "\nCorrect: " + count);
+            alert("Total: " + this.state.questions.length + "\nAttempt: " + Object.keys(answers).length + "\nCorrect: " + countCorrect);
           }}
         >
           {({ answers }) => (
