49eebafbccb266b60b334657f20bb28be1c02695
QuanPT <quanpt@gmail.com>
2021-05-21 01:59:20 +1000

    added actual question content

--- src/components/print.tsx
+++ src/components/print.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import { toJSON } from 'cssjson';
 import useScript from '../hooks/useScript';
+import { JSDOM } from 'jsdom'
+import createDOMPurify from 'dompurify'
 
 // interfaces
 export interface QuizI {
@@ -9,7 +11,7 @@ export interface QuizI {
     title: string;
 }
 
-function CSStoJSON(css:string) {
+function CSStoJSON(css: string) {
     return toJSON(css).attributes
 }
 
@@ -19,8 +21,8 @@ function PrintCoverPage() {
     </div>
 }
 
-function PrintBlankPage(props: {page: number}) {
-    return <div className="stl_ stl_02"  key="page02">
+function PrintBlankPage(props: { page: number }) {
+    return <div className="stl_ stl_02" key="page02">
         {/* blank page */}
         <div className="stl_view">
             <div className="stl_05 stl_06">
@@ -33,39 +35,55 @@ function PrintBlankPage(props: {page: number}) {
     </div>
 }
 
-function PrintQuestionSets(props: {questionSets: any}) {
+function PrintQuestionSets(props: { questionSets: any }) {
     return <>
         {props.questionSets.map((pair: any, index: number) => {
-        return <PrintQuestionPage pair={pair} index={index} key={"set_" + index} />
+            return <PrintQuestionPage pair={pair} index={index} key={"set_" + index} />
         })}
     </>
 }
 
-function PrintQuestionPage(props: {pair: any, index: number}) {
+function PrintQuestionPage(props: { pair: any, index: number }) {
     return <div className="stl_ stl_02" key={"page_" + props.index}>
-                <div className="stl_view">
-                    <span className="HeadSpace">&nbsp;</span>
-                    {
-                        [...new Array(props.pair.length)].map((_,i) => i).map((i) => {
-                            return <PrintQuestion question={props.pair[i]} index={props.index} n={i} key={props.index * 2 + i} />
-                        })
-                    }
-                    <div className="stl_01" style={CSStoJSON("left:24.5309em;top:66.0238em;")}>
-                        <span className="stl_24 stl_08 stl_11">{props.index + 3}</span>
-                    </div>
-                </div>
+        <div className="stl_view">
+            <span className="HeadSpace">&nbsp;</span>
+            {
+                [...new Array(props.pair.length)].map((_, i) => i).map((i) => {
+                    return <PrintQuestion question={props.pair[i]} index={props.index} n={i} key={props.index * 2 + i} />
+                })
+            }
+            <div className="stl_01" style={CSStoJSON("left:24.5309em;top:66.0238em;")}>
+                <span className="stl_24 stl_08 stl_11">{props.index + 3}</span>
             </div>
+        </div>
+    </div>
 }
 
-function PrintQuestion(props: {question: any, index: number, n: number}) {
-    
+let reImage1 = new RegExp(/\$image1\$/g);
+// const window = (new JSDOM('')).window
+// const DOMPurify = createDOMPurify(window)
+
+function PrintQuestion(props: { question: any, index: number, n: number }) {
+
+    let q = props.question
+    let key = q.id
+    var rawHtml = q.question
+    rawHtml = rawHtml.replace(/\r?\n|\r/g, '')
+        .replace(/^.*\s*<hr\s*size="1"\/>/gi, '')
+        .replace('<br/> <br/> <br/></div>', '</div>')
+        .replace(/\ssrc="/g, '  class="questionImage" src="/assets/')
+        .replace(reImage1, '<img src="/assets/figures/' + props.question.mmfid + '_1.jpg" class="questionImage" />')
+        .replace('<a href="show_image.html?name=', '<a href="/assets/')
+        .replace('target="ReadingText"', 'target="_blank"')
+    if (q.imageId) {
+        rawHtml = '<img src="/assets/articles/bigfish/' + q.imageId + '.jpg" />' + rawHtml;
+    }
+
     return <div className="stl_05" key={"question_" + props.index + "_" + props.n}>
-            <span className="QuestionNumber">{props.index * 2 + props.n + 1}</span>
-            <span className="QuestionText">
-                {JSON.stringify(props.question.qgroup)}
-            </span>
-            { props.n === 1 && <span className="AnswerOption">&nbsp; </span> }
-        </div>
+        <span className="QuestionNumber">{props.index * 2 + props.n + 1}</span>
+        <span className="QuestionText" dangerouslySetInnerHTML={{ __html: rawHtml}} />
+        {props.n === 1 && <span className="AnswerOption">&nbsp; </span>}
+    </div>
 }
 
 export function PrintableQuiz(props: QuizI) {
@@ -83,7 +101,7 @@ export function PrintableQuiz(props: QuizI) {
         fetch("/data/questions/get", {
             method: 'POST',
             headers: { 'Content-Type': 'application/json; charset=UTF-8' },
-            body: JSON.stringify({title: title})
+            body: JSON.stringify({ title: title })
         })
             .then(res => res.json())
             .then(
@@ -117,7 +135,7 @@ export function PrintableQuiz(props: QuizI) {
             <PrintCoverPage />
             <PrintBlankPage page={2} />
             <PrintQuestionSets questionSets={questionSets} />
-            <PrintBlankPage page={32}/>
+            <PrintBlankPage page={32} />
         </>
     )
 }
